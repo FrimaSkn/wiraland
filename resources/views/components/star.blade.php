0843d1e25f2856ebe444081cf925df9fee8d12b2
Frima Skn <frimasatria347@gmail.com>
2025-11-27 16:22:53 +0700

    Remove XML declaration from star component SVG

--- resources/views/components/star.blade.php
+++ resources/views/components/star.blade.php
@@ -6,7 +6,6 @@
 <div class="flex gap-4 {{ $position }}">
     @for($i = 1; $i <= $star; $i++)
         <div class="size-10">
-            <?xml version="1.0" encoding="UTF-8"?>
                 <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22.72 21.66">
                 <defs>
                     <style>
