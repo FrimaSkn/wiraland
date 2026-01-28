2fd1b6fe24653ebc2be3815f40e4f7eecfc9b810
Frima Skn <frimasatria347@gmail.com>
2026-01-28 15:12:53 +0700

    fix: Remove commented-out email subscription form from footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,26 +6,6 @@
         <div class="flex max-lg:flex-col justify-between">
             <div class="max-lg:mb-8">
                 <p class="text-white mt-8">Kami adalah Penyedia jasa pembuatan taman <br> terbaik untuk semua wilayah se-INDONESIA</p>
-                {{-- <form action="{{ route('subscribe') }}" method="POST">
-                    @csrf
-                    <div class="relative mt-4">
-                        <input type="text" name="email" class="block w-full text-white p-3 pe-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-xl border-secondary focus:ring-brand focus:border-brand shadow-xs placeholder:text-white" placeholder="Tulis email kami disini..." required />
-                        <button type="submit" class="absolute end-1.5 bottom-1.5 text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded text-xs focus:outline-none">
-                            <img class="size-8" src="{{ asset('assets/svg/icon-arrow.svg') }}" alt="">
-                        </button>
-                    </div>
-                     @if (env('GOOGLE_RECAPTCHA_STATUS'))
-                        <div>
-                            <x-recaptcha />
-                            <input type="hidden" name="captcha_status" value="true" />
-                            @error('g-recaptcha-response')
-                                <small class="text-red-500">
-                                    {{ $message }}
-                                </small>
-                            @enderror
-                        </div>
-                    @endif
-                </form> --}}
                 <livewire:store-email-subscription />
             </div>
             <div class="flex gap-[20%] lg:w-[25%]">
