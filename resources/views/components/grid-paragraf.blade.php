e982e2d298993ee80c954788bca3ad3e8944f691
Frima Skn <frimasatria347@gmail.com>
2026-02-16 19:30:07 +0700

    feat: Add new font files and update styles for DM Sans and Roboto Condensed

--- resources/views/components/grid-paragraf.blade.php
+++ resources/views/components/grid-paragraf.blade.php
@@ -3,7 +3,7 @@
     'desc' => ''
 ])
 
-<div class="md:grid grid-cols-2 gap-[10%] container max-md:space-y-3">
+<div class="md:grid grid-cols-2 gap-[10%] container max-md:space-y-3 font-dm-sans">
     <div data-aos="fade-up" class="section-title !py-0">{{ $title }}</div>
     <div data-aos="fade-up">{!! nl2br($desc) !!}</div>
 </div>
